ac0a2e179d5c763d3fb2498a2bfd99c40979f0cb
Arkadius Jonczek <arkadius@jonczek.de>
2019-12-25 22:44:26 +0100

    refactor testing bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,5 @@
 <?php declare(strict_types=1);
 
-require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+define('DS', DIRECTORY_SEPARATOR);
+
+require __DIR__ . DS . '..' . DS . 'vendor' . DS . 'autoload.php';
